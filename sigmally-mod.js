8c6293b5bc9ddd0486138275ce88e3c33f7d83b8
toto8899 <127184094+toto8899@users.noreply.github.com>
2023-03-08 16:49:40 +0100

    Update sigmally-mod.js

--- sigmally-mod.js
+++ sigmally-mod.js
@@ -456,7 +456,7 @@ const rzSkins = [ {
 	"cost": 300,
 	"_id": "60296810c7a2c784e938fa7f",
 	"name": "Captain.png",
-	"category": "premium",
+	"category": "free",
 	"visible": true,
 	"createTime": "2021-02-14T18:12:32.981Z",
 	"updateTime": "2021-02-14T18:12:32.981Z",
